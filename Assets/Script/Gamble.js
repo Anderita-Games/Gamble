5c42bc03a946bf239575b66db216b6b8e8bd5c29
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-10 22:34:16 -0500

    Added App
    This is the first version of Gamble that GitHub has ever seen!

---
+++ Assets/Script/Gamble.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+var Pot : UnityEngine.UI.InputField;
+var Total : UnityEngine.UI.Text;
+var Addition;
+function Start () {
+	//vars
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.Escape)) {
+		Application.LoadLevel("MainMenu");
+	}
+}
+
+function Left () {
+	var VPot = int.Parse(Pot.text);
+	var VTotal = int.Parse(Total.text);
+	var number = Random.Range(0,2);
+	Debug.Log ("Number" + number);
+	if (VPot <= VTotal && number == 1) {
+		//Correct!!!
+		Addition = (VPot + VTotal);
+		VPot = Addition;
+		Total.text = Addition.ToString();
+		Debug.Log ("Total:" + VTotal);
+	}
+	else {
+		Addition = (VTotal - VPot);
+		VPot = Addition;
+		Total.text = Addition.ToString();
+		Debug.Log ("Total:" + VTotal);
+	}
+} 
+
+function Right () {
+	var VPot = int.Parse(Pot.text);
+	var VTotal = int.Parse(Total.text);
+	var number = Random.Range(0,2);
+	Debug.Log ("Number" + number);
+	if (VPot <= VTotal && number == 0) {
+			//Correct!!!
+		Addition = (VPot + VTotal);
+		VPot = Addition;
+		Total.text = Addition.ToString();
+		Debug.Log ("Total:" + VTotal);
+	}
+	else {
+		Addition = (VTotal - VPot);
+		VPot = Addition;
+		Total.text = Addition.ToString();
+		Debug.Log ("Total:" + VTotal);
+	} 
+}
---
+++ Assets/Script/MainMenu.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function QuitGame () 
+{
+	Debug.Log ("Game is exiting...");
+	Application.Quit ();
+}
+
+function StartGame () 
+{
+	Application.LoadLevel("Gamble");
+}
+
+function StartT () 
+{
+	Application.LoadLevel("");
+}
